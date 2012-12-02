52347f02580120204c25feb71b6b5967afbcab46
marcelduin <marcel@q42.nl>
2012-12-02 10:52:12 +0100

    auto-opens current condition with correct value on popup

--- popup.js
+++ popup.js
@@ -19,22 +19,42 @@ $('.accordion-header a').click(function () {
 	return false;
 });
 
-$('#ret-pigmentosa a').bind('click focus', function(){return setCover('images/Retinitis-pigmentosa-'+this.textContent+'.png')});
-$('#diabetic-ret a').bind('click focus', function(){return setCover('images/diabetische-retinopathie-'+this.textContent+'.png')});
-$('#glaucoom a').bind('click focus', function(){return setCover('images/glaucoom-'+this.textContent+'.png')});
-$('#macula-deg a').bind('click focus', function(){return setCover('images/macula-degeneratie-'+this.textContent+'.png')});
+var $ret = $('#ret-pigmentosa a').bind('click focus', function(){return setCover('images/Retinitis-pigmentosa-'+this.textContent+'.png',this)});
+var $dia = $('#diabetic-ret a').bind('click focus', function(){return setCover('images/diabetische-retinopathie-'+this.textContent+'.png',this)});
+var $gla = $('#glaucoom a').bind('click focus', function(){return setCover('images/glaucoom-'+this.textContent+'.png',this)});
+var $mac = $('#macula-deg a').bind('click focus', function(){return setCover('images/macula-degeneratie-'+this.textContent+'.png',this)});
 $('#slider-protanomaly,#slider-deutanomaly,#slider-tritanomaly,#slider-achromatopsy,#slider-cataract').change(changeSlider);
 
 refreshSliders();
 
-function setCover(url) {chrome.extension.sendMessage({action:'cover',value:url});return false};
+function setCover(url,_) {
+	chrome.extension.sendMessage({action:'cover',value:url});
+	$(_).addClass('active').parent().siblings().children('a').removeClass('active');
+	return false;
+};
 function changeSlider(p) {var p=this.id.replace('slider-','');chrome.extension.sendMessage({action:p,value:this.value});refreshSliders()};
 function giveElementFocus(elt){$('[tabindex="0"]').attr('tabindex','');elt.attr('tabindex','0')};
 function refreshSliders() {
 	chrome.extension.sendMessage({action:'getCurrentSettings'},function(d){
-		var cover = d.cover;
-		delete d.cover;
-		for(var x in d) document.getElementById('slider-'+x).value=d[x];
+		for(var x in d) {
+			var $e = null;
+			if(x=='cover') {
+				var n = d.cover.replace(/^\".*-(\d)\.png\"$/,'$1');
+				if(/pigmentosa/.test(d.cover)) $e=$ret;
+				else if(/diabetische/.test(d.cover)) $e=$dia;
+				else if(/glaucoom/.test(d.cover)) $e=$gla;
+				else if(/macula/.test(d.cover)) $e=$mac;
+				if(n&&$e&&d[x]) $e.filter('a:contains('+n+')').addClass('active');
+			}
+			else {
+				$e = $('#slider-'+x);
+				$e[0].value=d[x];
+			}
+			if(d[x]) {
+				var $ct = $e.closest('.accordion-content');
+				if(!$ct.hasClass('open-content')) $ct.prev().children('a').click();
+			}
+		}
 	});
 };
 
